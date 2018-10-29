268c0aa106d23a417bb40b32b34a6ccba7628b7c
aroarian <aroarian@hotmail.com>
2018-10-29 19:36:29 -0400

    works?

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
-app.use(express.static("./client/build"));
+
+
+app.use(express.static("./client"));
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+
+app.listen(PORT, function() {
+    console.log("App listening on Port " + PORT )
+});   
