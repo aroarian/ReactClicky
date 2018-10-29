2eeef70df1530b67937bd2bb4ca279f8fc2b79b8
aroarian <aroarian@hotmail.com>
2018-10-29 18:40:31 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,80 +1,6 @@
 import React from "react";
 import "./App.css";
-import img1 from "./images/1.png";
-import img2 from "./images/2.png";
-import img3 from "./images/3.png";
-import img4 from "./images/4.png";
-import img5 from "./images/5.png";
-import img6 from "./images/6.png";
-import img7 from "./images/7.png";
-import img8 from "./images/8.png";
-import img9 from "./images/9.png";
-import img10 from "./images/10.png";
-import img11 from "./images/11.png";
-import img12 from "./images/12.png";
-
-let images = [
-  {
-    id: 1,
-    photo: img1,
-    clicked: false
-  },
-  {
-    id: 2,
-    photo: img2,
-    clicked: false
-  },
-  {
-    id: 3,
-    photo: img3,
-    clicked: false
-  },
-  {
-    id: 4,
-    photo: img4,
-    clicked: false
-  },
-  {
-    id: 5,
-    photo: img5,
-    clicked: false
-  },
-  {
-    id: 6,
-    photo: img6,
-    clicked: false
-  },
-  {
-    id: 7,
-    photo: img7,
-    clicked: false
-  },
-  {
-    id: 8,
-    photo: img8,
-    clicked: false
-  },
-  {
-    id: 9,
-    photo: img9,
-    clicked: false
-  },
-  {
-    id: 10,
-    photo: img10,
-    clicked: false
-  },
-  {
-    id: 11,
-    photo: img11,
-    clicked: false
-  },
-  {
-    id: 12,
-    photo: img12,
-    clicked: false
-  }
-];
+import images from "./images.json"
 
 class App extends React.Component {
   state = {
@@ -105,37 +31,36 @@ class App extends React.Component {
     const image = this.state.images;
     const imageClicked = image.filter(image => image.id === id);
 
+    
+
     if (imageClicked[0].clicked === true) {
       alert("Game Over!");
-      this.state.score = 0;
       this.gameEnd(images)
       this.shuffleArray(images);
-      this.forceUpdate();
+      this.setState({images: this.state.images, score: 0});
+     
     } 
     else {
+      
       imageClicked[0].clicked = true;
       this.state.score = this.state.score + 1;
       this.shuffleArray(images);
-      this.forceUpdate();
-      
-    }
+      this.setState({images: this.state.images});
+    };
 
     if (this.state.score >= this.state.topScore) {
-      this.state.topScore = this.state.score;
-    }
+      this.setState({topScore: this.state.score});
+    };
 
-    //use id to select out of the array the one i clicked
-    //update clicked property to true
-    //functionality to determine if true or false. if true then you have clicked it twice. if false then update to true and imcrement your score.
+    if (this.state.topScore === 12){
+      alert("You Win!")
+      this.setState({score: 0, topScore: 0});
+    };
+
   };
   render() {
     const displayImages = this.state.images.map((eachItem, index) => (
-      <img
-        alt="click me!"
-        onClick={() => this.handleImgClick(eachItem.id)}
-        key={index}
-        src={eachItem.photo}
-      />
+      <img alt="click me!" onClick={() => this.handleImgClick(eachItem.id)} key={index} src={eachItem.photo} />
     ));
 
     return (
