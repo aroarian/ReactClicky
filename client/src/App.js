f8bc74b600a16fcd22195037efff58884ad0a25e
aroarian <aroarian@hotmail.com>
2018-10-29 19:28:29 -0400

    Last

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import images from "./images.json"
+import images from "./images.json";
 
 class App extends React.Component {
   state = {
@@ -31,46 +31,50 @@ class App extends React.Component {
     const image = this.state.images;
     const imageClicked = image.filter(image => image.id === id);
 
-    
-
     if (imageClicked[0].clicked === true) {
       alert("Game Over!");
-      this.gameEnd(images)
+      this.gameEnd(images);
       this.shuffleArray(images);
-      this.setState({images: this.state.images, score: 0});
-     
-    } 
-    else {
-      
+      this.setState({ images: this.state.images, score: 0 });
+    } else {
       imageClicked[0].clicked = true;
       this.state.score = this.state.score + 1;
       this.shuffleArray(images);
-      this.setState({images: this.state.images});
-    };
+      this.setState({ images: this.state.images });
+    }
 
     if (this.state.score >= this.state.topScore) {
-      this.setState({topScore: this.state.score});
-    };
+      this.setState({ topScore: this.state.score });
+    }
 
-    if (this.state.topScore === 12){
-      alert("You Win!")
-      this.setState({score: 0, topScore: 0});
-    };
-
+    if (this.state.topScore === 12) {
+      alert("You Win!");
+      this.setState({ score: 0, topScore: 0 });
+    }
   };
   render() {
     const displayImages = this.state.images.map((eachItem, index) => (
-      <img alt="click me!" onClick={() => this.handleImgClick(eachItem.id)} key={index} src={eachItem.photo} />
+      <img
+        alt="click me!"
+        onClick={() => this.handleImgClick(eachItem.id)}
+        key={index}
+        src={eachItem.photo}
+      />
     ));
 
     return (
       <div className="App">
         <nav className="nav">
-          <div className="scores">
-            <h2 id="score">Score: {this.state.score}</h2>
-            <h2 id="topscore">TopScore: {this.state.topScore}</h2>
+        
+          <div className="content">
+              <h3>ClickyGame!</h3>
+            <div className="scores">
+              <div id="score">Score: {this.state.score}</div>
+              <div id="topscore">TopScore: {this.state.topScore}</div>
+            </div>
           </div>
         </nav>
+        <div id="rules">Click an images to begin!</div>
         <div className="images">{displayImages}</div>
       </div>
     );
