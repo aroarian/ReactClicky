d1eb254080a15ef497cc05ad4e3c5460dfe0f554
aroarian <aroarian@hotmail.com>
2018-10-29 20:21:29 -0400

    update

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 
 
-app.use(express.static("./client"));
+app.use(express.static("./client/src"));
 const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, function() {
     console.log("App listening on Port " + PORT )
-});   
+});
