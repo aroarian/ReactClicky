f172366dcd5d558684ef69a917bb3f96dfcd9d35
aroarian <aroarian@hotmail.com>
2018-11-01 17:20:40 -0400

    build

---
+++ application/build/precache-manifest.30c432c07a6a86a498601e1cceb560b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c98914392115f85f6101",
+    "url": "/ReactClicky/static/js/runtime~main.c9891439.js"
+  },
+  {
+    "revision": "e11ea730b9cd9d2e6c52",
+    "url": "/ReactClicky/static/js/main.e11ea730.chunk.js"
+  },
+  {
+    "revision": "d7c3259394c4b01131ba",
+    "url": "/ReactClicky/static/js/1.d7c32593.chunk.js"
+  },
+  {
+    "revision": "e11ea730b9cd9d2e6c52",
+    "url": "/ReactClicky/static/css/main.86d0c97e.chunk.css"
+  },
+  {
+    "revision": "71bcdead9c702b143b16ab34a9c8dbca",
+    "url": "/ReactClicky/index.html"
+  }
+];
---
+++ application/build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ReactClicky/precache-manifest.30c432c07a6a86a498601e1cceb560b4.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ReactClicky/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
